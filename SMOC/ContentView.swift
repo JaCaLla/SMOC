f8ac2d74136b9e9e7bce40831d6ef918fffd86f1
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-01-13 18:19:46 +0100

    Blank ios project

---
+++ SMOC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SMOC
+//
+//  Created by Javier Calatrava on 13/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SMOC/SMOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMOCApp.swift
+//  SMOC
+//
+//  Created by Javier Calatrava on 13/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SMOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
